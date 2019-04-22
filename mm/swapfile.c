974e4bb7d71bfdf9c3eaae655f326e9bc4c01bcc
Lukas Bulwahn <lukas.bulwahn@gmail.com>
2019-04-22 08:25:05 +0200

    mm/swapfile: annotate swap_{start,stop}
    HANDLES:
    
    mm/swapfile.c:2615:1: warning: mutex 'swapon_mutex' is still held at the end of function [-Wthread-safety-analysis]
    }
    ^
    mm/swapfile.c:2602:2: note: mutex acquired here
            mutex_lock(&swapon_mutex);
            ^
    mm/swapfile.c:2639:2: warning: releasing mutex 'swapon_mutex' that was not held [-Wthread-safety-analysis]
            mutex_unlock(&swapon_mutex);
            ^

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2595,7 +2595,7 @@ static __poll_t swaps_poll(struct file *file, poll_table *wait)
 }
 
 /* iterator */
-static void *swap_start(struct seq_file *swap, loff_t *pos)
+static void *swap_start(struct seq_file *swap, loff_t *pos)  __acquires_mutex(swapon_mutex)
 {
 	struct swap_info_struct *si;
 	int type;
@@ -2636,7 +2636,7 @@ static void *swap_next(struct seq_file *swap, void *v, loff_t *pos)
 	return NULL;
 }
 
-static void swap_stop(struct seq_file *swap, void *v)
+static void swap_stop(struct seq_file *swap, void *v) __releases_mutex(swapon_mutex)
 {
 	mutex_unlock(&swapon_mutex);
 }
